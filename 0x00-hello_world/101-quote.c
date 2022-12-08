8455eef219022877a84f6a9ae760d1e178c21130
PraiseJah Ossai <praisejah.moses22@gmail.com>
2022-12-08 12:34:00 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
 int main(void)
 {
 	write("and that piece of art is useful\" "
-"- Dora Korpar, 2015-10-19\n", 59, 1, STDOUT_FILEO);
+"- Dora Korpar, 2015-10-19\n", 59, 1, STDOUT_FILENO);
 	return (1);
 }
