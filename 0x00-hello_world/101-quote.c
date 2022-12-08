7a504916a56032b51145be44e5e9fb9c2af8c4b9
PraiseJah Ossai <praisejah.moses22@gmail.com>
2022-12-08 12:19:33 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-        write(STDOUT_FILENO, "and that piece of art is useful" 
-               "\" - Dora Korpar, 2015-10-19 \n ", 59);
+	fwrite("[stderr]:and that piece of art is useful"
+	       "\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
 	return (1);
 }
