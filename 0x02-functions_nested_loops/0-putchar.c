4db0ba9168ebdae3149854726bc3b60676d5ebc2
PraiseJah Ossai <praisejah.moses22@gmail.com>
2022-12-13 15:20:15 -0800

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,7 +11,7 @@ int main(void)
 	char word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int i;
 
-	for (i = 0; i < sizeof 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(word[i]);
 	}
